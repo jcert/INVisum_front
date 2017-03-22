26a5bbea5d02968fcd53a75e74b38e091f9e2d54
jcert <jgcertorio@uol.com.br>
2017-03-22 15:57:04 -0300

    new page that allows you to remove previously seleced sets with ease

---
+++ src/pages/in-visum-search-list/in-visum-search-list.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+import { FakeItems, ApiTalker, SetSelect} from '../../providers/providers';
+import { InVisumSearchResultPage } from '../in-visum-search-result/in-visum-search-result';
+import { Dataset } from '../../models/dataset';
+
+
+@Component({
+  selector: 'in-visum-search-list',
+  templateUrl: 'in-visum-search-list.html'
+})
+export class InVisumSearchListPage {
+  currentItems: Dataset[] = [];
+  errorString: string;
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public items: FakeItems, public api: ApiTalker, public set: SetSelect) {
+    //this.set = navParams.get('set');
+    this.currentItems = set.get();
+    
+  }
+
+  shorten(x){ let max: any = 60; return (x.length > max)? x.substr(0, max-3)+"..." : x; }
+
+  /**
+   * Perform a service for the proper items.
+   */
+
+  /**
+   * Navigate to the detail page for this item.
+   */
+  openItem(item: Dataset) {
+    this.navCtrl.push(InVisumSearchResultPage, {
+      item: item
+    });
+  }
+
+}
