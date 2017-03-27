145fc41bac12ebb4bf0baa8cb9b3073216562583
jcert <jgcertorio@uol.com.br>
2017-03-27 01:09:02 -0300

    must change the ationSheets to ion-select, in case of big arrays. Not done with it yet

--- src/pages/in-visum-operate/in-visum-operate.ts
+++ src/pages/in-visum-operate/in-visum-operate.ts
@@ -13,13 +13,16 @@ import { Dataset } from '../../models/dataset';
 export class InVisumOperatePage {
   currentItems: Dataset[] = [];
   errorString: string;
+  
   opList: any = {
-      Slice  : {chosenName : 'Slice',  args : [['set', null, "sFs" ] ,['cond',null, (x) => {} ],['condExp',null, (x) => {} ]]},
-      Sort   : {chosenName : 'Sort',   args : [['set', null, "sFs" ] ,['col' ,null, (x) => {} ],['cond',null, (x) => {} ],['condExp',null, (x) => {} ]]},
-      Filter : {chosenName : 'Filter', args : [['set', null, "sFs" ] ,['col' ,null, (x) => {} ],['expr',null, (x) => {} ]]},
-      Merge  : {chosenName : 'Merge',  args : [['set1',null, "sFs" ],['set2',null, "sFs" ]]},
-      Join   : {chosenName : 'Join',   args : [['set1',null, "sFs" ],['set2',null, "sFs" ],['col1',null, (x) => {} ],['col2',null, (x) => {} ]]}
+      Slice  : {chosenName : 'Slice',  args : [['set',  "sFset", true ] ,['cond', 'sFcond', true ],['condExp', 'getNumber', true ]]},//left,right,step
+      Sort   : {chosenName : 'Sort',   args : [['set',  "sFset", true ] ,['col' , 'sFcol', true ],['cond', 'sFcond', true ],['condExp', (x) => {}, true ]]},//
+      Filter : {chosenName : 'Filter', args : [['set',  "sFset", true ] ,['col' , 'sFcol', true ],['expr', (x) => {}, true ]]},//
+      Merge  : {chosenName : 'Merge',  args : [['set1', "sFset", true ],['set2', "sFset", true ]]},
+      Join   : {chosenName : 'Join',   args : [['set1', "sFset", true ],['set2', "sFset", true ],['col1', 'sFcol', true ],['col2', 'sFcol', true ]]}
     }
+  opKeys: any = Object.keys(this.opList);
+  
   standardName : string = "operação";
   chosen : boolean = false;
   command : any = {op:null,args:{}};
@@ -28,15 +31,61 @@ export class InVisumOperatePage {
               public api: ApiTalker, public sets: SetSelect, public actionSheetCtrl: ActionSheetController) {
   }
   
+  getCol(item?:Dataset){
+    return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];//should return the columns of a given dataset
+  }
   
   functionThatUsesThis(reg:string,op:any) {
     this[op](reg);
   }
   
-  sFs(setField:string) { //funtion to select from sets
+  sFcol(colField:string) { //select from column
+    let conditions : any = this.getCol();
     let makeActionSelectSet = (name:string, field:string) => {
       return {text: name,
-              handler: () => {console.log(name+' clicked');this.command.args[field]=name;console.log(this.command.args[setField])}
+              handler: () => {this.command.args[field]=name}
+              };
+    };
+    let result: any = [];
+    
+    for(let c of conditions) {
+      result.push(makeActionSelectSet(c,colField));  
+    }
+    result.push({text: 'Cancelar',role: 'cancel',handler: () => {}});      
+    
+    let actionSheet = this.actionSheetCtrl.create({
+      title: colField,
+      buttons: result
+    });
+    actionSheet.present();
+  }
+  
+  
+  sFcond(condField:string) { //select from condition
+    let conditions : any = ['igual','diferente','maior','menor'];
+    let makeActionSelectSet = (name:string, field:string) => {
+      return {text: name,
+              handler: () => {this.command.args[field]=name}
+              };
+    };
+    let result: any = [];
+    
+    for(let c of conditions) {
+      result.push(makeActionSelectSet(c,condField));  
+    }
+    result.push({text: 'Cancelar',role: 'cancel',handler: () => {}});      
+    
+    let actionSheet = this.actionSheetCtrl.create({
+      title: condField,
+      buttons: result
+    });
+    actionSheet.present();
+  }
+  
+  sFset(setField:string) { //funtion to select from sets
+    let makeActionSelectSet = (name:string, field:string) => {
+      return {text: name,
+              handler: () => {this.command.args[field]=name}
               };
     };
     let result: any = [];
@@ -44,10 +93,10 @@ export class InVisumOperatePage {
     for(let set of this.sets.get()) {
       result.push(makeActionSelectSet(set.title,setField));  
     }
-    result.push({text: 'Cancelar',role: 'cancel',handler: () => {console.log('Cancelar clicked');}});      
+    result.push({text: 'Cancelar',role: 'cancel',handler: () => {}});      
     
     let actionSheet = this.actionSheetCtrl.create({
-      title: 'Conjuntos',
+      title: setField,
       buttons: result
     });
     actionSheet.present();
@@ -55,7 +104,7 @@ export class InVisumOperatePage {
 
   
   makeActionSelectOp(name:string) {
-    return {text: name,handler: () => {console.log(name+' clicked');this.command.op=name}};
+    return {text: name,handler: () => {this.command.op=name}};
   }
   
   presentActionSheet() {
@@ -68,7 +117,7 @@ export class InVisumOperatePage {
         this.makeActionSelectOp('Merge'),
         this.makeActionSelectOp('Filter'),
         this.makeActionSelectOp('Join'),
-        {text: 'Cancelar',role: 'cancel',handler: () => {console.log('Cancelar clicked');}}]});
+        {text: 'Cancelar',role: 'cancel',handler: () => {}}]});
     actionSheet.present();
   }
 
